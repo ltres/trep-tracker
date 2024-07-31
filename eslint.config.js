6c68baa0e71088176c1e7ce9ee55216d87c18006
Lorenzo Tresoldi LTres <75213944+ltres@users.noreply.github.com>
2024-07-31 23:23:12 +0200

    ESLint configs

--- eslint.config.js
+++ eslint.config.js
@@ -4,13 +4,20 @@ import tseslint from "typescript-eslint";
 
 
 export default [
-  { 
+  {
     files: ["src/**/*.{js,mjs,cjs,ts}"],
     ignores: ["dist/**", "build/**", "node_modules/**"],
+    rules: {
+      "indent": ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      "no-prototype-builtins":'off',
+      "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }],
+    },
   },
-  { 
-    languageOptions: { globals: globals.browser } 
+  {
+    languageOptions: { globals: globals.browser }
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {}
 ];
