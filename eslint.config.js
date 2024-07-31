79638a34e6a6890a0bbdeec31014f64c773523bd
Lorenzo Tresoldi LTres <75213944+ltres@users.noreply.github.com>
2024-07-31 21:32:23 +0200

    ESLint

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  { 
+    files: ["src/**/*.{js,mjs,cjs,ts}"],
+    ignores: ["dist/**", "build/**", "node_modules/**"],
+  },
+  { 
+    languageOptions: { globals: globals.browser } 
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
