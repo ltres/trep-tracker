ee81d6ae8bc37978a4a5a8b01887f1f002b90463
Lorenzo Tresoldi LTres <75213944+ltres@users.noreply.github.com>
2024-08-03 18:56:38 +0200

    Update versions script now bound to Husky
    Husky scripts
    
    Differend approach for auto version management
    
    Differend approach for auto version management
    
    Fix post commit
    
    Husky scripts
    
    Husky
    
    Test [patch+]
    
    Version adjustment
    
    Differend approach for auto version management
    
    Fix post commit
    
    Husky scripts
    
    Husky
    
    Version adjustment

--- git-hooks-scripts/commit-msg.cjs
+++ git-hooks-scripts/commit-msg.cjs
@@ -3,8 +3,7 @@
 // Update app versions in package.json and angular environments
 const fs = require('fs');
 const path = require('path');
-const versionInfo = require(path.join(__dirname, 'version.json'));
-const packageJson =  require(path.join(__dirname, 'package.json'));
+const versionInfo = require(path.join(__dirname, '..', 'version.json'));
 const { execSync } = require('child_process');
 
 // Read the commit message
@@ -25,9 +24,11 @@ if (majorBumpRegex.test(commitMsg)) {
     versionBump = 'patch';
   }
 
-console.log(`Commit msg: ${commitMsg}`)
+const versionFileStaged = execSync('git diff --name-only --cached').toString().trim();
 
-if (versionBump) {
+if (versionBump && versionFileStaged.indexOf('version.json') < 0) {
+    console.log(`Commit msg: ${commitMsg}`)
+
     // Read the current version
     let currentVersion = versionInfo.version;
 
@@ -74,55 +75,10 @@ if (versionBump) {
 
     // Update version.json
     versionInfo.version = newVersion;
-    fs.writeFileSync(path.join(__dirname, 'version.json'), JSON.stringify(versionInfo, null, 2));
+    fs.writeFileSync(path.join(__dirname, '..', 'version.json'), JSON.stringify(versionInfo, null, 2));
+    console.warn("Version.json updated and staged")
+    execSync(`git add ${path.join(__dirname, '..', 'version.json')}`)
 
-    updateApplicationFiles()
-
-    // Stage modified files
-    console.log(`Staging new files and amending commit`)
-
-    execSync(`git add ${path.join(__dirname, 'version.json')}`)
-    execSync(`git add ${path.join(__dirname, 'package.json')}`)
-    execSync(`git add ${path.join(__dirname, 'src','index.html')}`)
-    execSync(`git add ${path.join(__dirname, 'src','environments', '*')}`)
-
-    // Amend the commit
-    execSync('git commit --amend --no-edit');
-
-    // add the tag
-    execSync(`git tag -a v${versionInfo.version} -m "${versionBump} version bump" HEAD`);
-
-
-
-    console.log(`Version bumped to ${newVersion}`);
 }else{
     console.warn("No version bump requested")
 }
-
-
-function updateApplicationFiles(){
-    let filesToUpdate = [
-         path.join(__dirname, './src/environments/environment.ts'),
-         path.join(__dirname, './src/environments/environment.development.ts'),
-         path.join(__dirname, './src/environments/environment.electrified-dev.ts'),
-         path.join(__dirname, './src/environments/environment.electrified-prod.ts'),
-         path.join(__dirname, './src/environments/environment.github-pages.ts')
-    ];
-    
-    for (let file of filesToUpdate) {
-        console.info(`Updating version in ${file}`);
-        let data = fs.readFileSync(file, 'utf8');
-        data = data.replace(/userVersion: '(.*)'/, `userVersion: '${versionInfo.version}'`);
-        fs.writeFileSync(file, data);
-    }
-    
-    console.info(`Updating version in index.html`);
-    let data = fs.readFileSync(path.join(__dirname, 'src','index.html'), 'utf8');
-    data = data.replace(/v\d+\.\d+\.\d+-[a-z]+/g, `v${versionInfo.version}`);
-    fs.writeFileSync(path.join(__dirname, 'src','index.html'), data);
-    
-    packageJson.version = versionInfo.version;
-    console.info(`Updating version in package.json`);
-    fs.writeFileSync(path.join(__dirname, 'package.json'), JSON.stringify(packageJson, null, 2));
-}
-
