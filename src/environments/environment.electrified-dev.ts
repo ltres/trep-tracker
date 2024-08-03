adb54aff46bdce487f9aff745a80f23ee70667e9
Lorenzo Tresoldi LTres <75213944+ltres@users.noreply.github.com>
2024-08-03 11:53:50 +0200

    Version bump

--- src/environments/environment.electrified-dev.ts
+++ src/environments/environment.electrified-dev.ts
@@ -3,6 +3,6 @@ import { Environment } from '../types/types';
 
 export const environment: Environment = {
   storageService: ElectronService,
-  userVersion: '1.4.0-preview',
+  userVersion: '1.4.0-beta',
   environment: 'electrified-dev',
 };
