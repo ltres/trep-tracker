825305b75abc9f2189dae6d88880d907b2dbed97
Lorenzo Tresoldi LTres <75213944+ltres@users.noreply.github.com>
2024-11-03 22:00:53 +0100

    Version bump

--- src/environments/environment.electrified-dev.ts
+++ src/environments/environment.electrified-dev.ts
@@ -3,6 +3,6 @@ import{ Environment }from'../types/types';
 
 export const environment: Environment = {
   storageService: ElectronService,
-  userVersion: '2.4.0-beta',
+  userVersion: '2.4.1-beta',
   environment: 'electrified-dev',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ import{ Environment }from'../types/types';
 
 export const environment: Environment = {
   storageService: LocalFileStorageService,
-  userVersion: '2.4.0-beta',
+  userVersion: '2.4.1-beta',
   environment: 'default',
 };
